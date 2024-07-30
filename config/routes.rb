29f121fda38653ae8f51941a0a827fa0c94093e9
Artsiom Safonau <ArtemSafonau@ibagroup.eu>
2024-07-30 15:02:46 +0300

    Set user routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
+  get "users/new", to: "users#new", as: :new_user
 end
