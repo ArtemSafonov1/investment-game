ace81a64cfc2de30078606dbcc5284a7693963b9
Artsiom Safonau <ArtemSafonau@ibagroup.eu>
2024-07-30 16:12:13 +0300

    Add Market model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,27 @@
 class User < ApplicationRecord
+  has_one :market, dependent: :destroy
+
+  def buy_tokens(amount_of_dollars, market)
+    tokens_to_buy = amount_of_dollars / market.price
+    if capital >= amount_of_dollars
+      update(capital: capital - amount_of_dollars, amount_of_tokens: amount_of_tokens + tokens_to_buy)
+    else
+      false
+    end
+  end
+  
+  def sell_tokens(amount_of_dollars, market)
+    dollars_to_receive = amount_of_dollars * market.price
+    if amount_of_tokens >= amount_of_dollars
+      update(capital: capital + dollars_to_receive, amount_of_tokens: amount_of_tokens - amount_of_dollars)
+    else
+      false
+    end
+  end  
+
+  private
+
+  def create_market
+    Market.create(user: self, current_date: 5.years.ago, price: 10)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,12 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "users#index"
   get "users/new", to: "users#new", as: :new_user
+
+  resources :markets, only: [:show] do
+    member do
+      post 'buy', to: "markets#buy"
+      post 'sell', to: "markets#sell"
+      get 'next_date', to: "markets#next_date"
+    end
+  end
 end
